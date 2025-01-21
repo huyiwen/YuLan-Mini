3ec0c3d01f1fcff71a55c1faa893ce693417d239
huyiwen <1020030101@qq.com>
2025-01-21 22:58:48 +0800

    [moe] tokenize

--- pretrain/preprocess/tokenize/run_tokenize.sh
+++ pretrain/preprocess/tokenize/run_tokenize.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 data_path=$1
+dir_path=$(dirname $(realpath $0))
+job_id=$RANDOM
 
-tokenizer_path=<hf-tokenizer-path>
+source /data/pretrain-mini/.venv/bin/activate
+
+tokenizer_path=/data/pretrain-mini/preprocess/modify_tokenizer/1731-dropout
 num_file=10000
-num_worker=8
+num_worker=16
 # num_file means how many jsonl/json/parquet files to tokenize at once (to avoid memory overflow). If num_file < actual number of files, simply run the script multiple times to tokenize all files.
 
-export RAW_DATA_PREFIX="/data/raw"
-export INPUT_IDS_PREFIX="/data/input_ids"
+export RAW_DATA_PREFIX="/data/aa_raw"
+export INPUT_IDS_PREFIX="/data/aa_input_ids"
 # target save path for tokenized data. The tokenized data will retain the same directory structure as the raw data.
 
 echo num_file=$num_file num_worker=$num_worker
@@ -21,8 +25,7 @@ else
     echo $data_path
 fi
 
-
-python tokenize/tokenize_text.py \
+python $dir_path/tokenize_text.py \
     --tokenizer_path $tokenizer_path \
     --data_path $data_path \
     --model_name mini \
@@ -32,11 +35,12 @@ python tokenize/tokenize_text.py \
     --skip_exist True
 
 # split data by 0.01B tokens
-python tokenize/split_data.py $data_path
+datasets_to_delete=$dir_path/cache_$job_id.txt
+python $dir_path/split_data.py $datasets_to_delete $data_path
 
 # delete intermediate tokenization files
-cat datasets_to_delete.txt | xargs -I {} rm {}
-rm datasets_to_delete.txt
+cat $datasets_to_delete | xargs -I {} rm {}
+rm $datasets_to_delete
 
 # incase of missing deletion
 if [ -n "$(find . -type f -regex '.*part-[0-9]+\.jsonl')" ]; then
